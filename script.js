6240f095eb80ae4a0a3a617829d54c44d262ee1e
anabeatriz1807 <146956397+anabeatriz1807@users.noreply.github.com>
2024-08-02 09:26:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal=document.querySelector('caixa-principal');
+const caixaPergunta=document.querySelector('caixa-pergunta');
+const caixaResultado=document.querySelector('caixa-resultado');
+const caixaAlternativa=document.querySelector('caixa-alternativa');
+
+const pergunta=[
+    {
+        enunciado:"Pergunta 1",
+        Alternativas:[
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]
