6cc909f21b37f7eb359ccbb334b7cd4ada41239b
anabeatriz1807 <146956397+anabeatriz1807@users.noreply.github.com>
2024-10-03 07:48:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,45 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
         enunciado:"Você gosta da Inteligencia Artificial?",
-        alternativas:["Sim","Não"]
+        alternativas:[{
+            texto:"Sim",
+            afirmação:"Sim, porque me ajuda em alguns afazeres"
     },
     {
+        texto:"Não",
+        afirmação:"Não, pois não me ajuda em nada"
+    
+    }
+]
+    {
         enunciado:"Você usa a IA no seu dia a dia?",
-        alternativas:["Sim","Nâo"]
+        alternativas:[{
+            texto:"Sim",
+            afirmação:"Sim, uso no meu dia a dia"
+            },
+            {
+                texto:"Não"
+                afirmação:"Não uso no dia a dia"
+            }
+        ]
     },
     {
         enunciado:"Você considera a IA importante nos dias de hoje?",
-        alternativas:["Sim","Não"]
+        alternativas:[ {
+            texto:"Sim"
+            afirmação:"Considero importante"
+        },
+    {
+        texto:"Não"
+        afirmação:"Não considero importante"
     }
+        ]
+    }
+ }
 ]
 let posicao = 0;
 let perguntaAtual;
+
 
 function mostraPergunta(){
     perguntaAtual = perguntas[posicao];
@@ -29,6 +55,10 @@ function mostraAlternativas(){
     for(const alternativas of perguntaAtual.alternativas){
         const botaoAlternativa = document.createElement("button");
         botaoAlternativas.textContent = alternativas;
+        botaoAlternativa.addEventListener("click", funcion(){
+posicao++;
+mostraPergunta();
+        })
         caixaAlternativa.appendChild(botaoAlternativa);
     }
 }
