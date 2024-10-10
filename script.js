81778f83a8edbc19d2b73e39fffe5b754ea84a45
anabeatriz1807 <146956397+anabeatriz1807@users.noreply.github.com>
2024-10-10 07:34:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,61 +5,74 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
-        enunciado:"Você gosta da Inteligencia Artificial?",
-        alternativas:[{
-            texto:"Sim",
-            afirmação:"Sim, porque me ajuda em alguns afazeres"
-    },
-    {
-        texto:"Não",
-        afirmação:"Não, pois não me ajuda em nada"
-    
-    }
-]
-    {
-        enunciado:"Você usa a IA no seu dia a dia?",
-        alternativas:[{
-            texto:"Sim",
-            afirmação:"Sim, uso no meu dia a dia"
-            },
-            {
-                texto:"Não"
-                afirmação:"Não uso no dia a dia"
-            }
-        ]
-    },
-    {
-        enunciado:"Você considera a IA importante nos dias de hoje?",
-        alternativas:[ {
-            texto:"Sim"
-            afirmação:"Considero importante"
+        enunciado: "Você gosta da Inteligência Artificial?",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Sim, porque me ajuda em alguns afaeres"
         },
+        {
+            texto: "Não",
+            afirmação: "Não, pois não me ajuda em nada"
+        }
+        ]
+    },
     {
-        texto:"Não"
-        afirmação:"Não considero importante"
-    }
+        enunciado: "Você usa a IA no seu dia a dia?",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Sim, uso no dia a dia"
+        },
+        {
+            texto:"Não",
+            afirmação:"Não uso no meu dia a dia"
+        }
+        ]
+    },
+    {
+        enunciado: "Você considera a IA importante nos dias de hoje?",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Considero importante"
+        },
+        {
+            texto: "Não",
+            afirmação: "Não conidero importante"
+        }
         ]
     }
- }
 ]
 let posicao = 0;
 let perguntaAtual;
+let respostas = "";
 
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (posicao>=perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
-   mostraAlternativas();
-} 
-function mostraAlternativas(){
-    for(const alternativas of perguntaAtual.alternativas){
-        const botaoAlternativa = document.createElement("button");
-        botaoAlternativas.textContent = alternativas;
-        botaoAlternativa.addEventListener("click", funcion(){
-posicao++;
-mostraPergunta();
-        })
-        caixaAlternativa.appendChild(botaoAlternativa);
+    caixaAlternativa.textContent = " ";
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",  () => respostasSelecionadas(alternativa));
+        caixaAlternativa.appendChild(botaoAlternativas);
     }
+}
+function respostasSelecionadas(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmação;
+    respostas += afirmacoes + " ";
+    posicao++;
+    mostraPergunta();
+}
+function mostraResultado(){
+    caixaPergunta.textContent = "Confira suas respostas: ";
+    textoResultado.textContent = respostas; 
+    caixaAlternativa.textContent = "";
 }
 mostraPergunta();
